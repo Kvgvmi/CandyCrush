5e4d4090e9e17b063a4c35076791bfbbeaa997ad
kvgvmi <oucheikhimane7@gmail.com>
2024-03-21 00:41:17 +0000

    Update File1.js

--- File1.js
+++ File1.js
@@ -1,4 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    candyCrushGame();
+});
 
+function candyCrushGame() {
+    const grid = document.querySelector(".grid");
+    const scoreDisplay = document.getElementById("score");
+    const width = 8;
+    const squares = [];
+    let score = 0;
+    
     const candyColors = [
         "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/red-candy.png)",
         "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/blue-candy.png)",
