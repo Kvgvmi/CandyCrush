4e8452aa14a075f96339ec820468011b2b9b1b97
imane <125690385+Kvgvmi@users.noreply.github.com>
2024-03-21 00:13:53 +0000

    Add files via upload

---
+++ File1.js
@@ -0,0 +1,10 @@
+    const candyColors = [
+        "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/red-candy.png)",
+        "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/blue-candy.png)",
+        "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/green-candy.png)",
+        "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/yellow-candy.png)",
+        "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/orange-candy.png)",
+        "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/purple-candy.png)",
+    ];
+
+   
