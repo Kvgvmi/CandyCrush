a2f63599ec0d9aefcfe35046a86a84349e7e2c6b
kvgvmi <oucheikhimane7@gmail.com>
2024-03-21 00:26:50 +0000

    Update File1.js

--- File1.js
+++ File1.js
@@ -1,3 +1,4 @@
+   
     const candyColors = [
         "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/red-candy.png)",
         "url(https://raw.githubusercontent.com/kvgvmi/CandyCrush/master/Photos/blue-candy.png)",
