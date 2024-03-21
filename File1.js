f67fa367fa258dec3e6acb36e6798452fa62099d
kvgvmi <oucheikhimane7@gmail.com>
2024-03-21 00:50:31 +0000

    Update File1.js

--- File1.js
+++ File1.js
@@ -197,3 +197,44 @@ function checkColumnForFour() {
 }
 checkColumnForFour();
 
+//For Row of Three
+function checkRowForThree() {
+    for (i = 0; i < 61; i++) {
+        let rowOfThree = [i, i + 1, i + 2];
+        let decidedColor = squares[i].style.backgroundImage;
+        const isBlank = squares[i].style.backgroundImage === "";
+
+        const notValid = [
+            6,
+            7,
+            14,
+            15,
+            22,
+            23,
+            30,
+            31,
+            38,
+            39,
+            46,
+            47,
+            54,
+            55
+        ];
+        if (notValid.includes(i)) continue;
+
+        if (
+            rowOfThree.every(
+                (index) =>
+                    squares[index].style.backgroundImage === decidedColor &&
+                    !isBlank
+            )
+        ) {
+            score += 3;
+            scoreDisplay.innerHTML = score;
+            rowOfThree.forEach((index) => {
+                squares[index].style.backgroundImage = "";
+            });
+        }
+    }
+}
+checkRowForThree();
